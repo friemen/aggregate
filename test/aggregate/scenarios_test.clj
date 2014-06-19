abc2bbc3cd9179391aff21e3d8cbdf3eacd9bc57
falko <Falko.Riemenschneider@itemis.de>
2014-06-19 09:33:24 +0200

    Added scenario 2

--- test/aggregate/scenarios_test.clj
+++ test/aggregate/scenarios_test.clj
@@ -73,3 +73,73 @@
     (is (= 0 (record-count @db-con :c)))
     (is (= 1 (agg/delete! er1 @db-con a)))
     (is (= 0 (record-count @db-con :a)))))
+
+
+
+;; -------------------------------------------------------------------
+;; Scenario 2
+;; A ->1 B ->mn C, none owned
+
+
+(def schema2
+  [:b [(id-column)
+       [:name "varchar(30)"]]
+   :a [(id-column)
+       [:name "varchar(30)"]
+       (fk-column :b false)]
+   :c [(id-column)
+       [:name "varchar(30)"]]
+   :b_c [(fk-column :b false)
+         (fk-column :c false)]])
+
+(def er2
+  (agg/make-er-config
+   (agg/entity :a {}
+               (agg/->1 :b :b {:owned? false}))
+   (agg/entity :b {}
+               (agg/->mn :cs :c))
+   (agg/entity :c {}
+               (agg/->mn :bs :b {:query-fn (agg/make-query-<many>-fn :b :b_c :c_id :b_id)
+                                 :update-links-fn (agg/make-update-links-fn :b_c :c_id :b_id)}))))
+
+
+(deftest scenario2-test
+  (create-schema! @db-con schema2)
+  (let [saved-a (agg/save! er2 @db-con :a {:name "A"
+                                           :b {:name "B1"
+                                               :cs [{:name "C1"}
+                                                    {:name "C2"}]}})]
+    (testing "Did everything arrive in DB?"
+      (is (= 1 (record-count @db-con :a)))
+      (is (= 1 (record-count @db-con :b)))
+      (is (= 2 (record-count @db-con :c)))
+      (is (= 2 (record-count @db-con :b_c))))
+    (testing "Delete A, but Bs and Cs must survive"
+      (agg/delete! er2 @db-con saved-a)  
+      (is (= 0 (record-count @db-con :a)))
+      (is (= 1 (record-count @db-con :b)))
+      (is (= 2 (record-count @db-con :c)))
+      (is (= 2 (record-count @db-con :b_c))))
+    (testing "Delete B, but Cs must survive"
+      (agg/delete! er2 @db-con (-> saved-a :b))
+      (is (= 0 (record-count @db-con :b)))
+      (is (= 2 (record-count @db-con :c)))
+      (is (= 0 (record-count @db-con :b_c)))))
+  (let [saved-a (agg/save! er2 @db-con :a {:name "A"
+                                           :b {:name "B2"
+                                               :cs [{:id 1 :name "C1"}
+                                                    {:id 2 :name "C2"}]}})]
+    (testing "Add another C within A"
+      (agg/save! er2 @db-con (-> saved-a :b (update-in [:cs] conj {:name "C3"})))
+      (is (= 3 (record-count @db-con :c)))
+      (is (= 3 (record-count @db-con :b_c))))
+    (testing "Add another B, B3 and B2 share C3"
+      (agg/save! er2 @db-con :b {:name "B3"
+                                 :cs [{:id 3 :name "C3"}
+                                      {:name "C4"}]})
+      (is (= 4 (record-count @db-con :c)))
+      (is (= 5 (record-count @db-con :b_c)))
+      (is (= 3 (-> (agg/load er2 @db-con :b 2) :cs count)))
+      (is (= 2 (-> (agg/load er2 @db-con :b 3) :cs count)))
+      (is (= 2 (-> (agg/load er2 @db-con :c 3) :bs count))))))
+  
