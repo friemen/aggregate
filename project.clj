4883f9c132294971de3c058c6018c8b6a33ead73
falko <Falko.Riemenschneider@itemis.de>
2014-10-14 15:05:42 +0200

    Fixed wrong test scope for java.jdbc

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject aggregate "1.0.1-SNAPSHOT"
+(defproject aggregate "1.0.1"
   :description "Persisting complex datastructures in SQL tables"
   :url "https://github.com/friemen/aggregate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [parsargs "1.2.0"]
-                 [org.clojure/java.jdbc "0.3.3" :scope "test"]
+                 [org.clojure/java.jdbc "0.3.3"]
                  [java-jdbc/dsl "0.1.0" :scope "test"]
                  [com.h2database/h2 "1.4.178" :scope "test"]
                  [org.postgresql/postgresql "9.3-1101-jdbc4" :scope "test"]]
